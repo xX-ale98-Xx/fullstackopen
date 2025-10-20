097498244d6ba8e18b01b4439cd1f15a6ed39fd3
alewaya <a.nicastro@wayassauto.com>
2025-10-20 23:52:57 +0200

    Finish part 2.11. - Phonebook step 6 - Add fetching from fake server logic.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import Form from './components/Form'
 import List from './components/FilteredList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+
+  useEffect(() => {
+    
+    console.log('effect');
+    console.log('fetching data...');
+    setTimeout(() => {
+      axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Fetched. The response is: ', response.data);
+        setPersons(response.data);
+      });
+    }, 1000);
+
+    
+  
+
+  }, []);
+
+  const [persons, setPersons] = useState([]); 
   const [newName, setNewName] = useState('');
   const [filterName, setFilterName] = useState('');
   const [newPhone, setNewPhone] = useState('');
