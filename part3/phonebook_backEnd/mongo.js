4cd9fa884221373bbb02e0589f1a5135fea31485
alewaya <a.nicastro@wayassauto.com>
2026-02-14 18:30:37 +0100

    Add logic for connecting to mongo database and saving data to it.

---
+++ part3/phonebook_backEnd/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://nicaale98_db_user:${password}@cluster0.pwilaux.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
